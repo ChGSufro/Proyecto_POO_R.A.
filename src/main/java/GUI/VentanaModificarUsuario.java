e253e613621acb8b7002643ac3a40382ddc6ee77
chris <c.gajardo08@ufromail.cl>
2023-12-09 16:42:06 -0300

    detalles

--- src/main/java/GUI/VentanaModificarUsuario.java
+++ src/main/java/GUI/VentanaModificarUsuario.java
@@ -328,10 +328,12 @@ public final class VentanaModificarUsuario extends VentanaAbstractRA implements 
                         field_contraseñaNueva.getText().replace(" ", "").isEmpty() ||
                         field_confirmarContraseña.getText().replace(" ", "").isEmpty()){
                     JOptionPane.showMessageDialog(null, "Ingrese correctamente todos los campos,\n los espacios en blanco no cuentan.");
+                    return;
                 }
 
                 if (!gestorDeClientes.contraseñaCorrecta(cliente, field_contraseñaActual.getText())){
-                    JOptionPane.showMessageDialog(null, "Contraseña incorrecta.");return;
+                    JOptionPane.showMessageDialog(null, "Contraseña incorrecta.");
+                    return;
                 }
                 if (gestorDeClientes.modificarContraseña(cliente, field_contraseñaNueva.getText(), field_confirmarContraseña.getText())) {
                     JOptionPane.showMessageDialog(null, "Contraseña modificada correctamente.");
