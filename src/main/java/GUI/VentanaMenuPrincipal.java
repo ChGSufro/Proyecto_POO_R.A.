c3e0d10c0835490a3349950ca0a1b25724254adf
javier <j.alcalde01@ufromail.cl>
2023-12-09 22:15:19 -0300

    documentacion

--- src/main/java/GUI/VentanaMenuPrincipal.java
+++ src/main/java/GUI/VentanaMenuPrincipal.java
@@ -9,6 +9,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Ventana Principal despues del Login, donde se muestran los botones que puede realizar el usuario.
+ */
 public final class VentanaMenuPrincipal extends VentanaAbstractRA implements ActionListener {
 
     private JPanel panel;
@@ -17,6 +20,11 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
     private GestorDeCabañas gestorDeCabañas;
     private Cliente usuarioIngresado;
 
+    /**
+     * Constructor de la ventana, que setea las caracteristicas de esta, recibe el gestor de clientes y cliente logeado.
+     * @param gestorDeClientes Encargado de administrar los clientes del sistema.
+     * @param usuarioIngresado Usuario logeado actualmente en el sistema.
+     */
     public VentanaMenuPrincipal(GestorDeClientes gestorDeClientes, Cliente usuarioIngresado){
         this.gestorDeClientes = gestorDeClientes;
         this.gestorDeCabañas = new GestorDeCabañas(this.gestorDeClientes);
@@ -59,6 +67,10 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         setContentPane(fondo);
     }
 
+    /**
+     * Metodo que crea el boton que dirige a la ventana donde se muestran todas las cabañas.
+     * @return Devuelve el JButton que el usuario puede presionar para ver todas las cabañas.
+     */
     private JButton crearBotonMostrarCabañas(){
         JButton boton = crearBoton("#EC9E48");
         boton.setText("Mostrar cabañas");
@@ -67,6 +79,10 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         return boton;
     }
 
+    /**
+     * Metodo que crea el boton que dirige a la ventana donde se arriendan las cabañas.
+     * @return Devuelve el JButton que el usuario puede presionar para arrendar cabañas.
+     */
     private JButton crearBotonArrendarCabaña(){
         JButton boton = crearBoton("#EC9E48");
         boton.setText("Arrendar cabaña");
@@ -75,6 +91,10 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         return boton;
     }
 
+    /**
+     * Metodo que crea el boton que dirige a la ventana donde se muestran todas las cabañas reservadas por el usuario logeado.
+     * @return Devuelve el JButton que el usuario puede presionar para ver todas las cabañas reservadas por el usuario actual.
+     */
     private JButton crearBotonVerReservas(){
         JButton boton = crearBoton("#EC9E48");
         boton.setText("Ver reservas");
@@ -83,6 +103,10 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         return boton;
     }
 
+    /**
+     * Metodo que crea el boton que dirige a la ventana donde el usuario puede realizar el checkout de sus cabañas.
+     * @return Devuelve el JButton que el usuario puede presionar para realizar el checkout de la cabaña.
+     */
     private JButton crearBotonCheckOut(){
         JButton boton = crearBoton("#EC9E48");
         boton.setText("Check-out");
@@ -91,6 +115,10 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         return boton;
     }
 
+    /**
+     * Metodo que crea el boton que cierra la sesion del usuario y guarda los cambios en los archivos Clientes.json y Cabañas.json.
+     * @return Devuelve el JButton que el usuario puede presionar para el cierre de la sesion.
+     */
     private JButton crearBotonCerrarSesion(){
         JButton boton = crearBoton("#047994");
         boton.setText("Cerrar sesión");
@@ -99,15 +127,19 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
         return boton;
     }
 
+    /**
+     * Metodo de la interfaz ActionListener que captura los eventos de la ventana.
+     * @param event El evento que ocurrió en la ventana.
+     */
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent event) {
 
-        if (e.getSource() == b_mostrarCabañas){
+        if (event.getSource() == b_mostrarCabañas){
             VentanaMostrarCabaña ventanaMostrarCabaña = new VentanaMostrarCabaña(gestorDeCabañas.getListaCabañas());
             ventanaMostrarCabaña.setVisible(true);
         }
 
-        if (e.getSource() == b_arrendarCabaña){
+        if (event.getSource() == b_arrendarCabaña){
             if (gestorDeCabañas.getCabañasDisponibles().isEmpty()) {
                 JOptionPane.showMessageDialog(null, "No hay cabañas disponibles");
             } else {
@@ -116,7 +148,7 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
             }
         }
 
-        if (e.getSource() == b_verReservas){
+        if (event.getSource() == b_verReservas){
             if (gestorDeCabañas.getCabañasReservadas(this.usuarioIngresado).isEmpty()){
                 JOptionPane.showMessageDialog(null, "No tiene cabañas reservadas");
 
@@ -126,7 +158,7 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
             }
         }
 
-        if (e.getSource() == b_checkOut){
+        if (event.getSource() == b_checkOut){
             if(gestorDeCabañas.getCabañasReservadas(this.usuarioIngresado).isEmpty()){
                 JOptionPane.showMessageDialog(null, "No tiene cabañas reservadas");
             }else{
@@ -135,7 +167,7 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
             }
         }
 
-        if (e.getSource() == b_cerrarSesion){
+        if (event.getSource() == b_cerrarSesion){
             dispose();
             this.gestorDeCabañas.registrarCabañasEnArchivoJson();
             this.gestorDeClientes.registrarClientesEnArchivoJson();
@@ -143,10 +175,9 @@ public final class VentanaMenuPrincipal extends VentanaAbstractRA implements Act
             menu.setVisible(true);
         }
 
-        if (e.getSource() == b_modificarUsuario){
+        if (event.getSource() == b_modificarUsuario){
             VentanaModificarUsuario menu = new VentanaModificarUsuario(this.usuarioIngresado, this.gestorDeClientes);
             menu.setVisible(true);
         }
-
     }
 }
